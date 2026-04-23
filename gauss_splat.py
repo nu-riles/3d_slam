3c10aa8f7c8bcdc1f4af8384f411bd7584cf36c9
nu-riles <iles.r@northeastern.edu>
2026-04-22 20:32:26 -0400

    Update gauss_splat.py

--- gauss_splat.py
+++ gauss_splat.py
@@ -245,7 +245,7 @@ def train_clip(
             quats     = F.normalize(model.quats, dim=-1),
             scales    = model.get_scales(),
             opacities = model.get_opacities(),
-            colors    = model.get_colors().unsqueeze(0).expand(len(dataset.frames), -1, -1).contiguous().to(device),
+            colors    = colors = model.get_colors().to(device),
             viewmats  = viewmats.contiguous().to(device),
             Ks        = Ks.contiguous().to(device),
             width     = W,
